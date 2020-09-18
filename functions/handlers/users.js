287b040162bab67a4d6e633a044c4dc0e115c42d
StevanZivlak <zivlaks@yahoo.com>
2020-09-18 15:22:10 +0200

    signup function changed to use async/wait

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -4,53 +4,47 @@ const firebase = require("firebase")
 
 firebase.initializeApp(config)
 
-// exports.signup = (req, res) => {
-//     const newUser = {
-//         userName: req.body.userName,
-//         email: req.body.email,
-//         password: req.body.password,
-//         confirmPassword: req.body.confirmPassword
-//     }
+exports.signup = async (req, res) => {
+    const newUser = {
+        userName: req.body.userName,
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword
+    }
 
-//     //TODO: entered data validation here
+    let userId, tokenVar
 
-//     let userId, tokenVar
+    try {
+        const user = await db.doc(`/users/${newUser.userName}`).get()
+        if (user.exists) {
+            return res.status(400).json({ userName: "This username already exists." })
+        }
+        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+        const data = await firebase
+            .auth()
+            .createUserWithEmailAndPassword(newUser.email, newUser.password)
 
-//     db.doc(`/users/${newUser.userName}`)
-//         .get()
-//         .then((user) => {
-//             if (user.exists) {
-//                 return res.status(400).json({ userName: "This username already exists." })
-//             } else {
-//                 return firebase
-//                     .auth()
-//                     .createUserWithEmailAndPassword(newUser.email, newUser.password)
-//             }
-//         })
-//         .then((data) => {
-//             userId = data.user.uid
-//         })
-//         .then((token) => {
-//             tokenVar = token
-//             const userCredentials = {
-//                 userName: newUser.userName,
-//                 email: newUser.email,
-//                 createdAt: new Date().toISOString(),
-//                 userId: userId
-//             }
-//             return db.doc(`/users/${newUser.userName}`).set(userCredentials)
-//         })
-//         .then(() => {
-//             return res.status(201).json({ token: tokenVar })
-//         })
-//         .catch((err) => {
-//             console.error(error)
-//             if (error.code === "auth/email-already-in-use") {
-//                 return res.status(400).json({ email: "Email is already in use" })
-//             } else {
-//                 return res
-//                     .status(500)
-//                     .json({ general: "Something went while trying to sign up. Please try again." })
-//             }
-//         })
-// }
+        userId = data.user.uid
+        const token = await data.user.getIdToken()
+
+        tokenVar = token
+        const userCredentials = {
+            userName: newUser.userName,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId: userId
+        }
+        await db.doc(`/users/${newUser.userName}`).set(userCredentials)
+        return res.status(201).json({ token: tokenVar })
+    } catch (err) {
+        console.error(err)
+
+        if (err.code === "auth/email-already-in-use") {
+            return res.status(400).json({ email: "Email is already in use" })
+        } else {
+            return res
+                .status(500)
+                .json({ general: "Something went while trying to sign up. Please try again." })
+        }
+    }
+}
